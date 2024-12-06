ed8b81913ee091a406844f4d35b29d517da6d530
super-kimchi <powercoding1004@gmail.com>
2024-12-06 18:46:38 +0100

    ft_printf start

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyungkim <kyungkim@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 13:30:36 by kyungkim          #+#    #+#             */
+/*   Updated: 2024/12/06 13:42:10 by kyungkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+
+int	ft_printf(const char *, ...);
+
+#endif
