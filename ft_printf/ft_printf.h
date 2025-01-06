ba4326abb618a8863f43cd7f93dfa4ce33573ce1
super-kimchi <powercoding1004@gmail.com>
2025-01-06 16:18:17 +0100

    upt

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kyungkim <kyungkim@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 13:30:36 by kyungkim          #+#    #+#             */
-/*   Updated: 2025/01/03 18:14:24 by kyungkim         ###   ########.fr       */
+/*   Updated: 2025/01/06 15:57:35 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_putchar(char c, int *count);
 void	ft_putstr(char *str, int *count);
 void	ft_puthex(unsigned long address, int *count, char c);
 void	ft_putnbr(long n, int *count);
+__attribute__((format(printf, 1, 2)))
 int		ft_printf(const char *text, ...);
-char	*ft_putpad(const char *text, int *count);
 
 #endif
